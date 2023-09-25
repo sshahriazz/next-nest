55158b41b2802b2f52d8ec36b47fb78d1f702a5d
Saifullah Siraji <shahriazkobir1994@hotmail.com>
2023-09-25 21:44:39 +0600

    remove server

--- apps/server/config/index.ts
+++
@@ -1,7 +0,0 @@
-export default () => ({
-  port: parseInt(process.env.PORT as string, 10) || 3000,
-  database: {
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT as string, 10) || 5432,
-  },
-});
